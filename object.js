b63da6e37ec0161330024455d89d042e7d4c97e5
Edwinmathaikooran <132889917+Edwinmathaikooran@users.noreply.github.com>
2023-05-08 14:32:33 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,11 @@
+let person ={
+    name:"Edu",
+    age:22,
+    place:"Neduvannor",
+    address:{
+        hname:"kooran",
+        postoffice:"chowara"
+    }
+};
+//console.log(person.address.hname)
+console.log("My name is ",person.name,"and my place is ",person.place)
---
+++ string.js
@@ -0,0 +1,6 @@
+let fname = "Edwin";
+let iname = "mathai";
+let fullname = fname+iname;
+//console.log(fullname);
+let names = `${fname}  ${iname}`;
+console.log(names);
