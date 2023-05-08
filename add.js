d98fab396b5e81ae87a22f0489d88404aef3d7fc
Edwinmathaikooran <132889917+Edwinmathaikooran@users.noreply.github.com>
2023-05-08 15:36:00 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,12 @@
+function add(){
+
+//    let c = a+b;
+//    console.log(c);   
+// }
+// add(5,10);
+// es6 syntax arrow function
+
+var add =(a,b)=>{
+    let c = a+b;
+    console.log(c);    
+}
